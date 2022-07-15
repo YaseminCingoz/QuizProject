0fa3c3c3b364939c121e4ae5c1fab519d650c195
Yasemin Cingoz <cingozyasemin@gmail.com>
2022-07-15 12:20:14 -0700

    Created Question class with an __init__()methof with two attributes.

---
+++ question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    #Created Question class with an __init__() method with two attributes text and answer attributes
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
